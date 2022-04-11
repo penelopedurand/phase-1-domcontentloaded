df497aac8030769b575e6dde71578d2ec107def3
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-04-11 13:26:52 -0400

    commit tree 86aeee2adb9231d3bda29a65aef61ce67c78ec13

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const newP = document.querySelector ('#text')
+function beLoaded () {
+    newP.textContent = "This is really cool!";
+}
+document.addEventListener ('DOMContentLoaded', beLoaded)
